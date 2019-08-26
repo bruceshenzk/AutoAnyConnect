577d52f3892ec61d0f06d90ec8abd2cc404fedc1
buszk <17345356+buszk@users.noreply.github.com>
2019-08-26 11:34:36 +0800

    update

--- aac.py
+++ aac.py
@@ -1,42 +1,68 @@
-#! /usr/bin/python
+#!/usr/bin/python
 from __future__ import print_function
 import sys,subprocess,os,time
+import argparse
+import logging
+logger = logging.getLogger("Auto-Anycisco-Connect")
+logger.setLevel("DEBUG")
 
-config_file = "vpn_config"
-cwd = os.getcwd()
+parser = argparse.ArgumentParser()
+parser.add_argument("config", action='store')
+args = parser.parse_args()
 
-# Check the config argument
-if len(sys.argv) > 1:
-	if os.path.isfile(cwd+"/"+sys.argv[1]):
-		config_file = sys.argv[1]
-	else:
-		print("Config file does not exist!")
-print("Using config file: " + config_file)
+def main():
+    if os.path.exists(args.config):
+        logger.info("Using config file", args.config)
+    else:
+        logger.warning("Not valid config")
+        return 1
+    
+    # read for credentials
+    with open(args.config,"r") as f:
+            host = f.readline()[:-1]
+            usr = f.readline()[:-1]
+            pw = f.readline()[:-1]
+    
+    # turn on Cisco Anyconnect VPN
+    #os.system("printf '"+usr+"\n"+pw+"\ny'|/opt/cisco/anyconnect/bin/vpn -s connect "+host)
+    p = subprocess.Popen("/opt/cisco/anyconnect/bin/vpn -s connect " + host, shell=True,
+            stdout=None, stdin=subprocess.PIPE)
 
-# read for credentials
-with open(cwd+"/"+config_file,"r") as f:
-	host = f.readline()[:-1]
-	usr = f.readline()[:-1]
-	pw = f.readline()[:-1]
-	
+    time.sleep(2)
+    print("sending usr")
+    p.stdin.write(usr+'\n')
+    
+    time.sleep(2)
+    print("sending pw")
+    p.stdin.write(pw+'\n')
+    
+    time.sleep(2)
+    print("sending confirm")
+    p.stdin.write('y\n')
 
-# turn on Cisco Anyconnect VPN
-os.system("printf '"+usr+"\n"+pw+"\ny'|/opt/cisco/anyconnect/bin/vpn -s connect "+host)
+    p.wait()
 
-# keep process alive and check connecting state regularly
-try:
-	while True:
-		time.sleep(20)
-		output = str(subprocess.Popen("/opt/cisco/anyconnect/bin/vpn status", shell=True, stdout=subprocess.PIPE).stdout.read())
-		if ">> state: Disconnected" in output:
-			print(">> Disconnected")
-		elif ">> state: Connected" in output:
-			print(">> Connected")
-		else:
-			print(">> Other")
-			print(output)
+    # keep process alive and check connecting state regularly
+    try:
+            while True:
+                    time.sleep(20)
+                    output = str(subprocess.Popen("/opt/cisco/anyconnect/bin/vpn status", shell=True, stdout=subprocess.PIPE).stdout.read())
+                    if ">> state: Disconnected" in output:
+                            print(">> Disconnected")
+                    elif ">> state: Connected" in output:
+                            print(">> Connected")
+                    else:
+                            print(">> Other")
+                            print(output)
 
-# Ctrl + C to disconnect
-except KeyboardInterrupt:
-	print("\nDisconnecting:\n")
-	os.system("/opt/cisco/anyconnect/bin/vpn disconnect")
+    # Ctrl + C to disconnect
+    except KeyboardInterrupt:
+            print("\nDisconnecting:\n")
+            p = subprocess.Popen("/opt/cisco/anyconnect/bin/vpn disconnect", shell=True)
+            p.wait()
+    
+
+if __name__ == "__main__" :
+    sys.exit(main())
+
+
